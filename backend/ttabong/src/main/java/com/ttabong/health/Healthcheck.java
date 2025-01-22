d15545f0594448c2a884ebe8657494c96035fd82
강재현 <recreate.kang@gmail.com>
2025-01-23 07:07:34 +0900

    feat: vue connection test with backend done

--- backend/ttabong/src/main/java/com/ttabong/health/Healthcheck.java
+++ backend/ttabong/src/main/java/com/ttabong/health/Healthcheck.java
@@ -1,9 +1,12 @@
 package com.ttabong.health;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("health")
+@RestController
 public class Healthcheck {
 
     @RequestMapping("")
--- frontend/ttabong/src/App.tsx
+++ frontend/ttabong/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import StatusCheck from "./StatusCheck"
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <StatusCheck /> {/* ✅ 백엔드 상태 확인 컴포넌트 추가 */}
     </>
   )
 }
